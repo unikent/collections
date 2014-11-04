8ba0f65c973d1495c55b4caf95470e2625f7bd02
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-04 10:02:55 +0000

    Move most config stuff to setup.php

--- config.example.php
+++ config.example.php
@@ -10,7 +10,7 @@ global $CFG;
 define("VERDI_INTERNAL", true);
 
 $CFG = new \stdClass();
-$CFG->brand = 'Rapid';
+$CFG->brand = 'VERDI';
 $CFG->dirroot = dirname(__FILE__);
 $CFG->cssroot = $CFG->dirroot . '/media/css';
 $CFG->jsroot = $CFG->dirroot . '/media/js';
@@ -18,50 +18,28 @@ $CFG->wwwroot = 'http://verdi-dev.kent.ac.uk:8080';
 $CFG->tilesize = 256;
 $CFG->cachedir = '/var/www/vhosts/verdi-dev.kent.ac.uk/writable/';
 
-require_once($CFG->dirroot . '/lib/corelib.php');
+$CFG->database = array(
+    'adapter' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'verdi',
+    'username' => 'root',
+    'password' => '',
+    'prefix' => 'v_'
+);
 
-// Register the autoloader now.
-spl_autoload_register(function($class) {
-	global $CFG;
+$CFG->cache = array(
+    'servers' => array(
+        array('localhost', '11211')
+    ),
+    'prefix' => 'verdi_'
+);
 
-	$parts = explode('\\', $class);
-
-	$filename = $CFG->dirroot . '/classes/' . implode('/', $parts) . '.php';
-	if (file_exists($filename)) {
-		require_once($filename);
-	}
-});
-
-// Register the composer autoloaders.
-require_once($CFG->dirroot . '/vendor/autoload.php');
-
-// DB connection.
-$DB = new \Rapid\Data\PDO('mysql', 'localhost', '3306', 'dbname', 'dbusername', 'dbpassword', 'table_prefix_');
-
-// Cache connection.
-$CACHE = new \Rapid\Data\Memcached(array(
-	array('localhost', '11211')
-), 'prefix_');
-
-// Start a session.
-//$SESSION = new \Rapid\Auth\Session();
-
-// Setup a guest user by default.
-//$USER = new \Rapid\Auth\User();
-
-// Output library.
-$OUTPUT = new \Rapid\Presentation\Output();
-
-// Page library.
-$PAGE = new \Rapid\Presentation\Page();
-
-// Set a default page title.
-$PAGE->set_title('Rapid Protoyping Framework');
-
-// Setup navigation.
-$PAGE->menu(array(
+$CFG->menu = array(
     'Home' => '/',
     'Zoomify' => '/demo/zoomify.php',
     'Formats' => '/demo/formats.php',
     'CALM' => '/demo/calm.php'
-));
+);
+
+require_once($CFG->dirroot . '/lib/setup.php');
---
+++ lib/setup.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Core Library.
+ *
+ * @package VERDI
+ * @subpackage lib
+ * @version 2.0
+ * @author Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @copyright University of Kent
+ */
+
+defined("VERDI_INTERNAL") || die("This page cannot be accessed directly.");
+
+require_once($CFG->dirroot . '/lib/corelib.php');
+
+// Register the autoloader now.
+spl_autoload_register(function($class) {
+    global $CFG;
+
+    $parts = explode('\\', $class);
+
+    $filename = $CFG->dirroot . '/classes/' . implode('/', $parts) . '.php';
+    if (file_exists($filename)) {
+        require_once($filename);
+    }
+});
+
+// Register the composer autoloaders.
+require_once($CFG->dirroot . '/vendor/autoload.php');
+
+// DB connection.
+$DB = new \Rapid\Data\PDO(
+    $CFG->database['adapter'],
+    $CFG->database['host'],
+    $CFG->database['port'],
+    $CFG->database['database'],
+    $CFG->database['username'],
+    $CFG->database['password'],
+    $CFG->database['prefix']
+);
+
+// Cache connection.
+$CACHE = new \Rapid\Data\Memcached($CFG->cache['servers'], $CFG->cache['prefix']);
+
+if (!defined('CLI_SCRIPT') || !CLI_SCRIPT) {
+    // Start a session.
+    //$SESSION = new \Rapid\Auth\Session();
+
+    // Setup a guest user by default.
+    //$USER = new \Rapid\Auth\User();
+
+    // Output library.
+    $OUTPUT = new \Rapid\Presentation\Output();
+
+    // Page library.
+    $PAGE = new \Rapid\Presentation\Page();
+
+    // Set a default page title.
+    $PAGE->set_title('VERDI');
+
+    // Setup navigation.
+    $PAGE->menu($CFG->menu);
+} else {
+    if (isset($_SERVER['REMOTE_ADDR']) || php_sapi_name() != 'cli') {
+        die("Must be run from CLI.");
+    }
+}
