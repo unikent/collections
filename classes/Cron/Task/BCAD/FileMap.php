6f6e169518d67d421c3462bda84b192c834cfa4f
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-01-27 11:12:42 +0000

    Update BCAD File Map cron

--- classes/Cron/Task/BCAD/FileMap.php
+++ classes/Cron/Task/BCAD/FileMap.php
@@ -22,24 +22,47 @@ class FileMap extends \Verdi\Cron\Task
         $list = $this->find_files($CFG->imageindir);
         
         // Delete any old entries from DB.
-        $entries = $DB->get_records('file_map');
-        foreach ($entries as $entry) {
-            $k = array_search($entry->fullpath, $list);
-            if ($k === false) {
-                echo "Removing '$entry->fullpath' from index...\n";
-                $DB->delete_records('file_map', $entry);
-            } else {
-                unset($list[$k]);
-            }
-        }
-        unset($entries);
+        $DB->execute("
+            DELETE f.*
+            FROM {bcad_files} f
+            LEFT OUTER JOIN {calm_catalog} c
+                ON c.recordid=f.id
+            WHERE c.id IS NULL
+        ");
+
+        $validfiles = array();
 
         // Add new ones.
         foreach ($list as $entry) {
-            echo "Adding '$entry' to index...\n";
-            $DB->insert_record('file_map', array(
-                'fullpath' => $entry
+            // Find related record.
+            $recordid = substr($entry, strrpos($entry, '/'));
+            do {
+                $recordid = substr($recordid, 0, strrpos($recordid, '.'));
+            } while (strrpos($recordid, '.') !== false);
+
+            // Find record.
+            $record = $DB->get_record('calm_catalog', array(
+                'refno' => $recordid
             ));
+
+            if ($record) {
+                echo "Adding '$entry' to index...\n";
+                $DB->insert_record('bcad_files', array(
+                    'recordid' => $record->id,
+                    'filename' => $entry
+                ));
+
+                $validfiles[$entry] = $record->id;
+            }
+        }
+
+        // Delete any entries from DB with no matching file.
+        $entries = $DB->get_records('bcad_files');
+        foreach ($entries as $entry) {
+            if (!isset($validfiles[$entry->filename])) {
+                echo "Removing '$entry->filename' from index...\n";
+                $DB->delete_records('bcad_files', $entry);
+            }
         }
     }
 
@@ -53,7 +76,7 @@ class FileMap extends \Verdi\Cron\Task
             $files = array_merge($files, $this->find_files($entry));
         }
 
-        $extensions = array('jpg', 'jpeg', 'tiff', 'gif', 'png');
+        $extensions = array('jpg', 'jpeg', 'tiff', 'tif', 'gif', 'png');
 
         $entries = glob($dir . "/*.*");
         foreach ($entries as $entry) {
