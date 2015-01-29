3e857e9a4bdfe89e213f23dd4bdcd621446dfcd9
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-01-29 11:47:41 +0000

    Add Cartoons SOLR client

---
+++ classes/Cron/Task/SOLR/Cartoons.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Image Processing Library.
+ *
+ * @package VERDI
+ * @subpackage lib
+ * @version 2.0
+ * @author Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @copyright University of Kent
+ */
+
+namespace Verdi\Cron\Task\SOLR;
+
+defined("VERDI_INTERNAL") || die("This page cannot be accessed directly.");
+
+class Cartoons extends \Verdi\Cron\Task
+{
+    public function do_run() {
+        global $CFG, $DB;
+
+        $client = new \Solarium\Client($CFG->solr);
+
+        $count = $DB->count_records('calm_catalog');
+        $batchsize = 100;
+        for ($i = 0; $i < $count; $i += $batchsize) {
+            $this->run_batch($client, $i, $batchsize);
+        }
+    }
+
+    /**
+     * Batch up.
+     */
+    private function run_batch($client, $start, $count) {
+        global $DB;
+
+        static $suffixes = array(
+            'id' => '',
+            'dayOfYear' => '_i',
+            'endDate' => '_i',
+            'isSingleDate' => '_i',
+            'startDate' => '_i'
+        );
+
+        $update = $client->createUpdate();
+
+        foreach ($DB->yield_records('calm_catalog', array(), '*', '', $count, $start) as $row) {
+            $doc = $update->createDocument();
+            foreach ((array)$row as $k => $v) {
+                if (isset($suffixes[$k])) {
+                    $k = $k . $suffixes[$k];
+                } else {
+                    $k = $k . '_t';
+                }
+
+                if (!empty($v)) {
+                    $doc->$k = $v;
+                }
+            }
+
+            $update->addDocument($doc);
+        }
+
+        $update->addCommit();
+
+        return $client->update($update);
+    }
+}
