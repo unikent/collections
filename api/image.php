2d81a078833723701aaf069585fcea265f63e6e4
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-02-02 10:39:35 +0000

    Separate zoomify and image servers

--- api/image.php
+++ api/image.php
@@ -10,52 +10,27 @@ require_once(dirname(__FILE__) . '/../config.php');
 $PAGE->set_url('/');
 $PAGE->set_title("Image Request");
 
-// We require a valid request.
-$request = required_param('request', PARAM_RAW_TRIMMED);
+$id = required_param('id', PARAM_INT);
+$size = required_param('size', PARAM_ALPHA);
 
-// The request is a mixture of an ID and a file request.
-list($id, $request) = explode('/', $request, 2);
+$image = new \SCAPI\Image\Processor($id);
 
 // This outputs a few different things.
-switch ($request) {
-    case "ImageProperties.xml":
-        header("Content-type: text/xml; charset=utf-8");
-        $image = new \SCAPI\Image\Processor($id);
-        echo $image->get_xml();
-
-        // Spawn a service to pre-process the other images.
-        $preprocessor = new \SCAPI\Service\PreProcessor();
-        $preprocessor->run(array(
-            "id" => $id,
-            'test' => 'test'
-        ));
+switch ($size) {
+    case "thumb":
+        $image->output_as(170, 0, 0, 170, 50);
     break;
 
+    case "print":
+        $image->output_as(3400, 0, 0, 3400, 60);
+    break;
+
+    case "full":
+        $image->output_as(3400, 0, 0, 3400, 40);
+    break;
+
+    case "standard":
     default:
-        $image = new \SCAPI\Image\Processor($id);
-
-        $parts = explode('/', $request);
-        switch ($parts[0]) {
-            case 'TileGroup0':
-                $tile = substr($parts[1], 0, strpos($parts[1], '.'));
-                $image->output_tile($tile);
-            break;
-
-            case "print":
-                $image->output_as(3400, 0, 0, 3400, 60);
-            break;
-
-            case "full":
-                $image->output_as(3400, 0, 0, 3400, 40);
-            break;
-
-            case "standard":
-                $image->output_as(482, 0, 0, 482, 60);
-            break;
-
-            case "thumb":
-                $image->output_as(170, 0, 0, 170, 50);
-            break;
-        }
+        $image->output_as(482, 0, 0, 482, 60);
     break;
 }
---
+++ api/zoomify.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Rapid Prototyping Framework in PHP.
+ * 
+ * @author Skylar Kelty <skylarkelty@gmail.com>
+ */
+
+require_once(dirname(__FILE__) . '/../config.php');
+
+$PAGE->set_url('/');
+$PAGE->set_title("Zoomify Request");
+
+// We require a valid request.
+$request = required_param('request', PARAM_RAW_TRIMMED);
+
+// The request is a mixture of an ID and a file request.
+list($id, $request) = explode('/', $request, 2);
+
+// This outputs a few different things.
+switch ($request) {
+    case "ImageProperties.xml":
+        header("Content-type: text/xml; charset=utf-8");
+        $image = new \SCAPI\Image\Processor($id);
+        echo $image->get_xml();
+
+        // Spawn a service to pre-process the other images.
+        $preprocessor = new \SCAPI\Service\PreProcessor();
+        $preprocessor->run(array(
+            "id" => $id,
+            'test' => 'test'
+        ));
+    break;
+
+    default:                   
+        $parts = explode('/', $request);
+        $tile = substr($parts[1], 0, strpos($parts[1], '.'));
+
+        $image = new \SCAPI\Image\Processor($id);
+        $image->output_tile($tile);
+    break;
+}
